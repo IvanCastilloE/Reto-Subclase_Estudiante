57ca525021e1578703e873eaefda26ba225d45d0
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-02-01 13:40:26 -0700

    Agregar archivos de proyecto.

---
+++ Subclase_Estudiante/Estudiante.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subclase_Estudiante
+{
+    class Estudiante:Persona
+    {
+        public double Promedio { get; set; }
+        public string Grado { get; set; }
+
+        public Estudiante(string nombre, int edad, double promedio, string grado): base(nombre,edad)
+        {
+            Promedio = promedio;
+            Grado = grado;
+        }
+
+        public virtual void Estudiar()
+        {
+            Console.WriteLine("Estoy estudiando duro en mi clase de programación");
+        }
+        public virtual void Grado_Decir()
+        {
+            Console.WriteLine("Mi semestre es: {0}",Grado);
+        }
+        public override void Saludar()
+        {
+            Console.WriteLine("Hola, soy {0} y mi grado es {1}", Nombre, Grado);
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}, {1} años, grado {2}, promedio {3}", Nombre, Edad, Grado, Promedio);
+        }
+    }
+}
---
+++ Subclase_Estudiante/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subclase_Estudiante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("El Babo", 38);
+            persona1.Saludar();
+
+            Estudiante estudiante1 = new Estudiante ("Timmy Turner", 18, 9.5, "2");
+            estudiante1.Saludar();
+            estudiante1.Estudiar();
+            estudiante1.Grado_Decir();
+            Console.ReadKey();
+        }
+    }
+}
